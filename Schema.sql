740a0e93057494a3db0ee35c3b2a04eb07cdcdff
DEBASISH BIHARI <debasisbihari287@gmail.com>
2024-03-11 14:43:05 +0530

    Add files via upload

---
+++ Schema.sql
@@ -0,0 +1,25 @@
+
+CREATE TABLE product_category (id INT PRIMARY KEY,
+                                              name VARCHAR NOT NULL,
+                                                           description TEXT, created_at TIMESTAMP,
+                                                                                        modified_at TIMESTAMP,
+                                                                                                    deleted_at TIMESTAMP);
+
+CREATE TABLE product (id INT PRIMARY KEY,
+                                     name VARCHAR NOT NULL,
+                                                  description TEXT, SKU VARCHAR, category_id INT, inventory_id INT, price DECIMAL, discount_id INT, created_at TIMESTAMP,
+                                                                                                                                                               modified_at TIMESTAMP,
+                                                                                                                                                                           deleted_at TIMESTAMP,
+                      FOREIGN KEY (category_id) REFERENCES product_category(id),
+                      FOREIGN KEY (inventory_id) REFERENCES product_inventory(id));
+
+CREATE TABLE discount (id INT PRIMARY KEY,
+                                      name VARCHAR NOT NULL,
+                                                   description TEXT, discount_percent DECIMAL, active BOOLEAN, created_at TIMESTAMP,
+                                                                                                                          modified_at TIMESTAMP,
+                                                                                                                                      deleted_at TIMESTAMP);
+
+CREATE TABLE product_inventory (id INT PRIMARY KEY,
+                                               quantity INT, created_at TIMESTAMP,
+                                                                        modified_at TIMESTAMP,
+                                                                                    deleted_at TIMESTAMP);
